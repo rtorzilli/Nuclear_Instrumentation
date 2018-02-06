afd1ac3cf0772a37b3f5377812992c02d756e627
Robert <torzillirobert@gmail.com>
2018-02-06 17:41:55 -0500

    for main pc

--- Gaseous_Detectors/week2/anaylsis_wk2.py
+++ Gaseous_Detectors/week2/anaylsis_wk2.py
@@ -26,19 +26,81 @@ N=len(FourB_data)
 
 # Experimental Mean X
 
-X = (FourB_data['Counts'].sum())/N
+X4B = (FourB_data['Counts'].sum())/N
 
+# Gauss Standard Deviation
+sigma = np.sqrt(X4B)
 
 #Sample Variance s^2 = VarSam
 # =============================================================================
 # How to get single value
 # print(FourB_data['Counts'][1])
 # =============================================================================
-VarSam_Top = 0
+
+def VarSam (givenDF,meanExp):
+    N=len(givenDF)
+    VarSam_Top = 0
+    for i in range(N):
+        VarSam_Top += (givenDF -meanExp)**2
+    VarSam=VarSam_Top/N    
+    return VarSam
+# =============================================================================
+# VarSam_Top = 0
+# for i in range(N):
+#     VarSam_Top += (FourB_data['Counts'][i] -X)**2
+# VarSam=VarSam_Top/N
+# =============================================================================
+varSam4B = VarSam(FourB_data,X4B)
+print('Experimental Mean: '+str(X4B))    
+print('Sample Variance: '+str(varSam4B))
+print('Standard Deviation s ~ ' +str(np.sqrt(varSam4B)) + ' Sigma ~ ' +str(sigma))
+
+# Refine data with 3sigma test
+# Define bounds
+lowBound=X4B-3*sigma
+upBound = X4B+3*sigma
+
+# Compare only keep if within bounds
+refined4B = []
 for i in range(N):
-    VarSam_Top += (FourB_data['Counts'][i] -X)**2
-VarSam=VarSam_Top/N
-
-print('Experimental Mean: '+str(X))    
-print('Sample Variance: '+str(VarSam))
-
+    if FourB_data['Counts'][i]<upBound and FourB_data['Counts'][i] > lowBound:
+        refined4B.append(FourB_data['Counts'][i])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
